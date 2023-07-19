2b5425410ef05a3be5a0fb9bdac5a80bbd7592af
maringrgatovic <56473997+maringrgatovic@users.noreply.github.com>
2023-07-19 15:57:35 +0200

    Add files via upload

---
+++ scripts/mobile.js
@@ -0,0 +1,7 @@
+var front = document.getElementById('card_face_front');
+
+front.innerHTML = '<img src="images/pozivnica_mobitel_smoljo_naprid.jpg">'
+
+var front = document.getElementById('card_face_back');
+
+front.innerHTML = '<img src="images/pozivnica_mobitel_smoljo_nazad.jpg">'
